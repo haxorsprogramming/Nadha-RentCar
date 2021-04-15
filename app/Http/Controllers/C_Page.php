4c946ae1ce60fc2ea99689dadf6850db897d6153
haxorsprogramming <haxorsuinsu@gmail.com>
2021-04-15 15:07:12 +0700

    'update'

---
+++ app/Http/Controllers/C_Page.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class C_Page extends Controller
+{
+    public function home()
+    {
+        echo "Halaman home";
+    }
+}
